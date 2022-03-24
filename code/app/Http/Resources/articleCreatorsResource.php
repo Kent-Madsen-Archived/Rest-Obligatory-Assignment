ba3abb6d9366b4144615ecfa72581f2f2b27dacd
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-03-24 13:37:53 +0100

    created request and resource classes

--- code/app/Http/Resources/articleCreatorsResource.php
+++ code/app/Http/Resources/articleCreatorsResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class accountResource extends JsonResource
+class articleCreatorsResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
